583760fda9e46b270ef7e47238c1b2634817c7a2
Flex58 <164533598+flex58@users.noreply.github.com>
2025-05-26 10:10:24 +0200

    style: add css modules baseline

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import PropTypes from 'prop-types'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import PropTypes from "prop-types";
+import classes from "./App.module.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
       <div>
         <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+          <img src={viteLogo} className={classes.logo} alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+          <img
+            src={reactLogo}
+            className={classes.logo + " " + classes.react}
+            alt="React logo"
+          />
         </a>
       </div>
       <h1>our first test</h1>
-      <div className="card">
+      <div className={classes.card}>
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
@@ -26,11 +30,11 @@ function App() {
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
+      <p className={classes["read-the-docs"]}>
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
