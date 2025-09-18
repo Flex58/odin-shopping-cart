8d4c6d4ccc9bea4d040ed185584e4190f3efd96c
Flex58 <164533598+Flex58@users.noreply.github.com>
2025-09-18 10:26:19 +0200

    refactor: refactor checkout to have item component

--- src/components/CheckOut.jsx
+++ src/components/CheckOut.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { useOutletContext } from "react-router-dom";
 import main from "../css/LayoutMain.module.css";
 import classes from "../css/CheckOut.module.css";
-import input from "../css/Cart.module.css";
+import CheckOutItem from "./CheckOutItem";
 
 function CheckOut() {
   const [, cart] = useOutletContext();
@@ -26,74 +26,43 @@ function CheckOut() {
     }
   }
 
+  function tPDec(item) {
+    setTotalPrice((c) => Number((c - item.price).toFixed(2)));
+  }
+
+  function tPInc(item) {
+    setTotalPrice((c) => Number((c + item.price).toFixed(2)));
+  }
+
+  function tPRemove(item) {
+    setTotalPrice((c) => Number((c - item.totalPrice).toFixed(2)));
+  }
+
   return (
     <div className={main.main + " " + classes.layout}>
       <div className={classes.wrapper}>
         {cart.cart.map((item) => {
           return (
-            <div key={item.id} className={classes.item}>
-              <img src={item.image} alt={"image of " + item.title} />
-              <h2>{item.title}</h2>
-              <p>{item.desc}</p>
-
-              <div className={input.input}>
-                <button
-                  type="button"
-                  onClick={() => {
-                    cart.decrement(item.id);
-                    setTotalPrice((c) => Number((c - item.price).toFixed(2)));
-                  }}
-                  disabled={item.amount <= 1 ? true : false}
-                  className={classes.btnOW + ' ' + classes.btnL}
-                >
-                  -
-                </button>
-                <input
-                  type="number"
-                  name="amount"
-                  id={"amount" + item.id}
-                  className={classes.inputOW}
-                  value={item.amount}
-                  onChange={(e) => handleChange(e, item)}
-                />
-                <button
-                  type="button"
-                  onClick={() => {
-                    cart.increment(item.id);
-                    setTotalPrice((c) => Number((c + item.price).toFixed(2)));
-                  }}
-                  className={classes.btnOW + " " + classes.btnR}
-                >
-                  +
-                </button>
-              </div>
-              <div className={classes.removeBtnDiv}>
-                <p>Price: {item.totalPrice}€</p>
-                <button
-                  type="button"
-                  onClick={() => {
-                    cart.removeFromCart(item.id);
-                    setTotalPrice((c) =>
-                      Number((c - item.totalPrice).toFixed(2))
-                    );
-                  }}
-                >
-                  x
-                </button>
-              </div>
-            </div>
+            <CheckOutItem
+              item={item}
+              cart={cart}
+              handleChange={handleChange}
+              tPInc={tPInc}
+              tPDec={tPDec}
+              tPRemove={tPRemove}
+            />
           );
         })}
       </div>
       {cart.cart.length === 0 ? (
-        <div>Cart is Empty</div>
+        <p>Cart is Empty</p>
       ) : (
-        <>
-          <div>Total Price: {totalPrice}€</div>
+        <div>
+          <p>Total Price: {totalPrice}€</p>
           <button type="button" onClick={() => cart.clearCart()}>
             Pay Now!
           </button>
-        </>
+        </div>
       )}
     </div>
   );
---
+++ src/components/CheckOutItem.jsx
@@ -0,0 +1,62 @@
+import classes from '../css/CheckOut.module.css'
+import input from '../css/Cart.module.css'
+
+function CheckOutItem({ item, cart, handleChange, tPInc, tPDec, tPRemove }) {
+  return (
+    <div key={item.id} className={classes.item}>
+      <img src={item.image} alt={"image of " + item.title} />
+      <h2>{item.title}</h2>
+      <button className={classes.description}>
+        <h3>Description</h3>
+        <hr />
+        <p>{item.desc}</p>
+      </button>
+
+      <div className={input.input}>
+        <button
+          type="button"
+          onClick={() => {
+            cart.decrement(item.id);
+            tPDec(item);
+          }}
+          disabled={item.amount <= 1 ? true : false}
+          className={classes.btnOW + " " + classes.btnL}
+        >
+          -
+        </button>
+        <input
+          type="number"
+          name="amount"
+          id={"amount" + item.id}
+          className={classes.inputOW}
+          value={item.amount}
+          onChange={(e) => handleChange(e, item)}
+        />
+        <button
+          type="button"
+          onClick={() => {
+            cart.increment(item.id);
+            tPInc(item);
+          }}
+          className={classes.btnOW + " " + classes.btnR}
+        >
+          +
+        </button>
+      </div>
+      <div className={classes.removeBtnDiv}>
+        <p>Price: {item.totalPrice}€</p>
+        <button
+          type="button"
+          onClick={() => {
+            cart.removeFromCart(item.id);
+            tPRemove(item);
+          }}
+        >
+          x
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default CheckOutItem;
